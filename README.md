d6d689be5088c312fa8c97f2a2228d6ea8583e3f
Jennifer Mack <jenn@jennifermack.net>
2015-03-02 14:59:17 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 # scalzi-generator
+
 A script that keeps John Scalzi's "Standard Responses to Online Stupidity" as close as a click.
 
 The original inspirational post can be found at: Scalzi's [Whatever blog](http://whatever.scalzi.com/2015/03/02/standard-responses-to-online-stupidity/).
+
+## Usage
+
+The script simply prints a random Response.
+
+    $ ./scalzi_generator.rb 
+    I didn't ask you.
+    $ ./scalzi_generator.rb 
+    I don't care what you think.
+
+On a Mac, the output can be sent to the clipboard.
+
+    $ ./scalzi_generator.rb | pbcopy
+
+## Workflow
+
+The `scalzi_generator.workflow.zip` unpacks into a workflow file that is used by the Automator app. From there it can saved as service for system wide Standard Response goodness.
---
+++ scalzi_generator.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+#
+scalzi = [                                                                       
+"I don't care what you think.",                                                  
+"I didn't ask you.",                                                             
+"No doubt you thought that was terribly clever.",                                
+"You've attempted logic. Not all attempts succeed",                              
+"One should not have that many errors in that few characters.",                  
+"Either your educators have failed you, or you have failed them.",               
+"I see you've invited me to an argument. I decline.",
+"It appears an asshole has hacked your account and is posting in your name.",    
+"Funny, most people go out of their way not to be a public bigot.",              
+"Cosplaying as a tantrum-throwing child is no way to go through life.",          
+"I'm sorry that you are so obviously scared of the world.",                      
+"My attention is a privilege, not a right. This is all you get."                 
+]
+
+puts scalzi.sample                                                              
+
